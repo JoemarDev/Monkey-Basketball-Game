f84238ab966b0b27d23036c70c1151f43080bd65
JoemarDev <JoemarB.Dev@gmail.com>
2020-12-10 15:00:17 +0800

    change the server link;

--- assets/js/script.js
+++ assets/js/script.js
@@ -832,7 +832,7 @@ function init() {
 		if (ss == 60) {
 			if (!animationIsPlaying) {
 				animationIsPlaying = true;
-				fetch('https://mt-police888.com//api/get-result/limit/10').then((res)=>{
+				fetch('https://mt-police888.com/api/get-result/limit/10').then((res)=>{
 					return res.json();
 				}).then((data) => {
 					game_rounds = data[0]['round'];
@@ -888,7 +888,7 @@ function init() {
 	})
 
 
-	fetch('https://mt-police888.com//api/get-result/limit/10').then((res)=>{
+	fetch('https://mt-police888.com/api/get-result/limit/10').then((res)=>{
 		return res.json();
 	}).then((data) => {
 		game_rounds = data[0]['round'];
